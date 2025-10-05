d9cd2bdcc87b2f4017d5f946bc79b30e0c64cb2d
Lemoneater027 <marufabdul4446@gmail.com>
2025-10-05 09:33:54 +0530

    all done

--- app.js
+++ app.js
@@ -137,7 +137,7 @@ const mockProducts = [
         name:  'Dumbbells',
         brsnd:  'FITPRO',
         originalPrice: 'Rs 999',
-        studentsPrice: 'Rs 799',
+        studentsPrice: 'Rs 699',
         discount: '20% OFF',
         image: 'https://images.unsplash.com/photo-1638536534847-2cb61af9efe3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         rating: 4.7,
